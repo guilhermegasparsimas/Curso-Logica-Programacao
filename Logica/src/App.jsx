58f0c46344647820d39a1a9de2718a3f5a8be717
GUILHERME GASPAR SIMAS <guilherme_g_simas@estudante.sesisenai.org.br>
2025-05-23 22:04:48 -0300

    acabou

--- Logica/src/App.jsx
+++ Logica/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function Adivinhar(){
+    let number = Math.ceil(Math.random()*10)
+    let palpite = Number(prompt("Chuta ai"))
+
+    if(palpite >= 1 && palpite <= 10 && palpite === number){
+      alert('Parabéns, você acertou')
+    }else{
+      alert('Não desista, tente novamente!')
+    }
+    console.log(number);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container-app'>
+      <h1>Acerte o número de 1 a 10</h1>
+
+      <div className="">
+        <button onClick={Adivinhar}>Adivinhar</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
