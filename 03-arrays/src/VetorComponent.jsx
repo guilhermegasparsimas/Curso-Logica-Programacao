fa1b2b7598d7d5a262153a895c815d5e0480c364
guilhermegasparsimas <guisimas0201@gmail.com>
2025-08-15 21:57:25 -0300

    lala

--- 03-arrays/src/VetorComponent.jsx
+++ 03-arrays/src/VetorComponent.jsx
@@ -2,21 +2,31 @@ import React, { useEffect, useState } from 'react';
 
 function VetorComponent() {
 
-    const [valor] = useState([1,2,3,4,5,6,7,8,9,10]);
-    const [valorInvertido, setValorInvertido] = useState([]);
+    // const [valor] = useState([1,2,3,4,5,6,7,8,9,10]);
+    // const [valorInvertido, setValorInvertido] = useState([]);
 
-    const converterIndices= () => {
-        const novoVetor = [];
+    // const converterIndices= () => {
+    //     const novoVetor = [];
 
-        for(let i=0; i < valor; i++){
-            novoVetor[i] = valor[valor.length -1 -i];
-        }
-        setValorInvertido(novoVetor);
-    }
+    //     for(let i=0; i < valor; i++){
+    //         novoVetor[i] = valor[valor.length -1 -i];
+    //     }
+    //     setValorInvertido(novoVetor);
+    // }
 
+          const [contador, setContador] = useState(0)
+        
+        
+            const incrementar = (contador) => {
+                 contador++
+                 setContador(contador)              
+                }
+
+            useEffect()
+                
   return (
     <>
-    <h2>Vetor Original</h2>
+    {/* <h2>Vetor Original</h2>
     <p>{valor.join(', ')}</p>     
     <button onClick={converterIndices}>Inverter Indices</button>
 
@@ -25,7 +35,9 @@ function VetorComponent() {
             <h1>Vetor com índices invertidos</h1>
             <p>{valorInvertido.join(', ')}</p>
         </>
-    )}
+    )} */}
+        <p>Valor: {contador}</p>
+    <button onClick={incrementar}>Incrementar</button> 
     </>
   )
 }
--- 03-arrays/src/dezValores.jsx
+++ 03-arrays/src/dezValores.jsx
@@ -2,31 +2,17 @@ import React from 'react'
 import { useState } from 'react'
 
 function dezValores() {
+    const [contador, setContador] = useState(0)
 
-    const [valor] = useState([1,2,3,4,5,6,7,8,9,10]);
-    const [valorInvertido, setValorInvertido] = useState([]);
 
-    const converterIndices= () => {
-        const novoVetor = [];
-
-        for(i=0; i < valor; i++){
-            novoVetor[i] = valor[valor.length -1 -i];
-        }
-        setValorInvertido(novoVetor);
+    const incrementar = (contador) => {
+        setContador(contador++)
     }
 
   return (
     <>
-    <h2>Vetor Original</h2>
-    <p>{valor.join(', ')}</p>     
-    <button onClick={converterIndices}>Inverter Indices</button>
-
-    {valor.length > 0 && (
-        <>
-            <h1>Vetor com índices invertidos</h1>
-            <p>{valorInvertido.join(', ')}</p>
-        </>
-    )}
+    <p>Valor: {contador}</p>
+    <button onClick={incrementar}></button> 
     </>
   )
 }
